2cd31653974213bbd5c42fda99642ed04ed7c0b1
alexcodeninja <aalexdeada@gmail.com>
2024-03-30 10:25:59 +0200

    memes

--- memes_set.py
+++
@@ -1,8 +0,0 @@
-import os
-import pygame
-
-literature_memes = set()
-folder_path = r"C:\Users\ученик\PycharmProjects\gogolmogol\memes"
-for filename in os.listdir(folder_path):
-    meme = pygame.image.load(f"{filename}")
-    literature_memes.add(meme)
